56f4dcb9cce74727e3af046d4c8c2f23e5a8e8e9
Lucia Rodriguez <774@holbertonschool.com>
2019-02-06 22:33:53 +0000

    trying the fourth task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+/**
+*puts - Printing a given line
+*
+*Description: This function prints a given line
+*Returns: The exercise requires it must be 0
+*/
+void puts(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
+
