949e17c595cd2a275d09ac0ffca22e63d1658352
Lucia Rodriguez <774@holbertonschool.com>
2019-02-07 22:00:26 +0000

    trying to upload again

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main -This code prints the alphabet in lowcase
+ *
+ *Description :We use a for cycle to get all the values for the alphabet into a
+ *variable, then we print it with a putchar function.
+ *
+ *Return : It is supposed to be 0
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
