619d9699bc4a764a8db3cce57d51804df9491c0b
Lucia Rodriguez <774@holbertonschool.com>
2019-02-07 22:37:05 +0000

    uploadin the 3d task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - We print the alphabet without e and q
+ *
+ *Description: We use a for to get all the alphabet set and after that we use
+ *if to find the e and the q and excludes from the list*
+ *
+ *Return: It is supossed to be 0
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a' ; alpha <= 'z'; alpha++)
+{
+if (alpha == 'e' || alpha == 'q')
+{
+alpha++;
+}
+
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
