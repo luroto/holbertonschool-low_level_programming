3fc9aedc10c1bda04c2a6cfd5218c98466ca7341
Lucia Rodriguez <774@holbertonschool.com>
2019-02-08 02:13:56 +0000

    trying to upload the tenth task

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - This function prints numbers from 00 to 99 in ASCII code
+ *
+ *Description: Using for to get data, if for conditionals
+ *
+ *Return: It is supposed to be 0
+ */
+int main(void)
+{
+int numa, numb;
+
+for (numa = 48 ; numa <= 57 ; numa++)
+{
+for (numb = 48 ; numb <= 58 ; numb++)
+{
+putchar(numa);
+putchar(numb);
+if (numb == 57 && numa == 57)
+{
+	numa++;
+	numb++;
+}
+if (numb < 58 && numa < 58)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
