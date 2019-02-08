8975457c8bb85d7fd587e2ef612a53a1c82b546b
Lucia Rodriguez <774@holbertonschool.com>
2019-02-08 02:23:15 +0000

    fixing the fourth

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,21 @@
-#include <stdio.h>
+ #include <stdio.h>
+/**
+ *main - We use this function to print the alphabet without two letters
+ *
+ *Return: It is supposed to be 0
+ */
 int main(void)
+{
+char alpha;
+
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+{
+if (alpha == 'e' || alpha == 'q')
+{
+alpha++;
+}
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
