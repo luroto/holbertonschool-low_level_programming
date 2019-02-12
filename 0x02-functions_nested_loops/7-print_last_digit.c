7e6d2f34000b3ee694e0ca0b7ad41b5e7629eac5
Lucia Rodriguez <774@holbertonschool.com>
2019-02-12 22:19:10 +0000

    uploading the 7th task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_last_digit - Prints the last digit of a number
+ *@a: stores input
+ *
+ *Return: It is supposed to be zero
+ */
+int print_last_digit(int a)
+{
+	int b;
+
+	b = a % 10;
+	if (b >= 0)
+	{
+		_putchar(b + '0');
+	}
+	else
+	{
+		 b = b * (-1);
+		   _putchar(b +'0');
+	}
+	return (b);
+}
