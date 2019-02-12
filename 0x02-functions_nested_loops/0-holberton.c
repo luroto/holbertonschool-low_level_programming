c000bd11cdd9dff9a45643c8a73e60b46155dab8
Lucia Rodriguez <774@holbertonschool.com>
2019-02-12 02:57:41 +0000

    Uploading the first task

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *main : We use this function to test _putchar
+ *
+ *Return: It is supposed to be 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
