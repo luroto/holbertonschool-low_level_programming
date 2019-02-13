f681a7280ad4b47982b9308b32032332c15c4506
Lucia Rodriguez <774@holbertonschool.com>
2019-02-13 02:13:48 +0000

    doing the tenth task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *add - Adds two integers and returns the result
+ *
+ *Return: The aftermath
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *times_tables - prints the 9 times table
+ *
+ *Return: it is suppossed to be 0
+ */
+void times_table(void)
+{
+	int h1, m1;
+	{
+		for(
