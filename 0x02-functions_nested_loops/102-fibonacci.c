ff053c729f5592bd64401aa7d95f7161b88287d3
Lucia Rodriguez <774@holbertonschool.com>
2019-02-14 02:25:45 +0000

    fixing the last output

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,18 +12,18 @@ long int f1, f2, f3;
 
 f1 = 0;
 f2 = 1;
-for(c = 0 ; c < 50 ; c++)
+for (c = 0 ; c < 49 ; c++)
 {
 f3 = f1 + f2;
 f1 = f2;
 f2 = f3;
-if( c == 49)
+if (c == 46)
 {
-	printf("%ld", f3);
+printf("%ld", f3);
 }
 else
 {
-	printf("%ld, ", f3);
+printf("%ld, ", f3);
 }
 }
 putchar('\n');
