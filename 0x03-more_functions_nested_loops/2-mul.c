e77acb61a9a69f290e312437af07e9de87ca0ca7
Lucia Rodriguez <774@holbertonschool.com>
2019-02-14 19:50:25 +0000

    uploading the second task

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,9 @@
  *
  *@a: Stores one integer
  *
- *@b: Stores the other one
- *
- *@d: contains the result
+ *@b: contains the result
  *
  *Return: It is supposed to be 0
- *
  */
 int mul(int a, int b)
 {
