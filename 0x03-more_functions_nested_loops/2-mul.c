fe34d1922bd0810f34d6823e17f2891eacc761b8
Lucia Rodriguez <774@holbertonschool.com>
2019-02-14 19:30:50 +0000

    fixing

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *mul - multiplies two integers
+ *
+ *@a: Stores one integer
+ *
+ *@b: Stores the other one
+ *
+ *Return: It is supposed to be 0
+ *
+ */
+int mul(int a, int b)
+{
+	_putchar(
