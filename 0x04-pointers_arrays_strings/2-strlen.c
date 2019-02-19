a93974615afa691fee19425aa55682f1abf36d9f
Lucia Rodriguez <774@holbertonschool.com>
2019-02-19 20:32:26 +0000

    Uploading the second task

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strlen - returns the length of a string.
+ *
+ *@s = it provides the length of the string.
+ *
+ *Return: The length of the string
+ *
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+return (l);
+}
---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *_puts - Print a string using a pointer
+ *
+ *Return: It is supposed to be 0
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+}
