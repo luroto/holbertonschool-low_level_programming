fdafb012c06c242131641976428c77b42e6159ad
Lucia Rodriguez <774@holbertonschool.com>
2019-02-20 01:55:05 +0000

    uploading the 8th task

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - prints n elements of an array
+ *
+ *@a: stores integers
+ *@n: number of elements
+ *
+ *Return: provided by exercise
+ */
+void print_array(int *a, int n)
+{
+	int l = 0;
+
+		while (l <= n)
+		{
+			printf("%d", a[l]);
+			printf(",");
+			printf(" ");
+			l++;
+		}
+		printf("\n");
+}
