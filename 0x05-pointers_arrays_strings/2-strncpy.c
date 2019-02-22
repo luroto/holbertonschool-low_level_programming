4a72b74c7003993bca9a596ca9c3d1d949d9456f
Lucia Rodriguez <774@holbertonschool.com>
2019-02-22 01:25:40 +0000

    uploading the 2nd task

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,20 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 0, isrc = 0;
 
-	while (i < n)
+	while (src[isrc])
+	{
+		isrc++;
+	}
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
