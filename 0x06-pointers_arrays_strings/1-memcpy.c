ef41460fabeb034d04d5de3420fcd3f6efe95f68
Lucia Rodriguez <774@holbertonschool.com>
2019-02-25 20:45:10 +0000

    Uploading the first task: copying n bytes from a source memory area to a dest memory area

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,29 @@
 #include "holberton.h"
 /**
- *_memcpy - copies n bytes from memory area src to memory area dest
+ **_memcpy - copies n bytes from memory area src to memory area dest
  *
  *@dest: destination memory area
  *@src: source memory area
+ *@n: number of bytes copied fron src
  *
  *Return: dest pointer
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i = 0, isrc = 0;
+
+	while (src[isrc])
+	{
+		isrc++;
+	}
+	if (n > isrc)
+	{
+		n = isrc;
+	}
 	while (i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	return(dest);
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -9,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
