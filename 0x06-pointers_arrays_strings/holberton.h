812bbed6bf8be5adb62bf02aa8c098034cb65814
Lucia Rodriguez <774@holbertonschool.com>
2019-02-26 02:49:46 +0000

    finally I was able to understand why my code wasn't working and I figured out how to fix it, uploading the third task

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -11,3 +11,4 @@ void print_square(int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
