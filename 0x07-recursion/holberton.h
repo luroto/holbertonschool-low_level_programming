91541867d2ebd929200e6887435e832568650736
Lucia Rodriguez <774@holbertonschool.com>
2019-02-27 22:06:45 +0000

    uploading the 0 task: printing a string

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,18 @@
+int _isupper(int c);
+int _putchar(char);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void _puts_recursion(char *s);
