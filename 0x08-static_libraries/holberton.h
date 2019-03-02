980a32c83405b3e429464c9c41e289fcd156064a
Lucía Rodríguez <774@holbertonschool.com>
2019-03-02 15:28:54 -0500

    fixing holberton.h

--- 0x08-static_libraries/holberton.h
+++ 0x08-static_libraries/holberton.h
@@ -1,24 +1,22 @@
 
 
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
 int _isupper(int c);
-int _putchar(char);
 int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
+char *_strstr(char *haystack, char *needle);
