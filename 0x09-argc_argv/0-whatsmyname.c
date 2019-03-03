0dd5b36098c1f4a2606a7a41e9b4a09b0cb2b5c3
Lucía Rodríguez <774@holbertonschool.com>
2019-03-03 14:54:04 -0500

    fixing betty and return

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main (int argc, char **argv)
+/**
+ *whatsmyname - Prints the name of the program
+ *
+ *@argc: counter of arguments
+ *@argv: name of arguments
+ *
+ *Return: = if succesful
+ */
+int whatsmyname(int argc, char **argv)
 {
 	if (argc > 0)
 	{
-		printf("%s",argv[0]);
+		printf("%s", argv[0]);
 		printf("\n");
 	}
 	return (0);
