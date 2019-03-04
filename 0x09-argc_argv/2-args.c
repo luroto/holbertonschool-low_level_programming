e3267a45f9e9859ad4a88034ba51b1cdf5144da0
Lucía Rodríguez <774@holbertonschool.com>
2019-03-03 21:26:03 -0500

    uploading the second task

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Prints the number of arguments passed into it
+ *
+ *@argc: number of arguments
+ *@argv: arguments
+ *Return: It is supposed to be 0
+ */
+int main (int argc, char **argv)
+{
+	int cont;
+
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	if (argc > 1)
+	{
+		for (cont = 0; cont < argc; cont++)
+		{
+			printf("%s\n", argv[cont]);
+		}
+	}
+	return (0);
+}
