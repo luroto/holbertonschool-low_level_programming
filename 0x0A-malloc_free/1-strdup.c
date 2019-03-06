1e7eaf94ca25a282f3cc70365ae6ab35ac281aee
Lucia Rodriguez <774@holbertonschool.com>
2019-03-06 00:53:20 +0000

    uploading the first task

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ **_strdup - copies a string on a newly allocated space
+ *
+ *@str: source string
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	unsigned int cont, contn;
+	char *nws;
+
+	for (cont = 0; str[cont]; cont++)
+	{
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	nws = malloc(sizeof(*nws) * cont + 1);
+	if (nws == NULL)
+	{
+		return (NULL);
+	}
+	for (contn = 0; contn <= cont; contn++)
+	{
+		nws[contn] = str[contn];
+	}
+	return(nws);
+}
