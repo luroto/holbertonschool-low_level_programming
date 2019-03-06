3e579997891ab35735678074fc0cf89a800b0d0a
Lucia Rodriguez <774@holbertonschool.com>
2019-03-06 03:29:53 +0000

    uploading the second task

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,2 +1,40 @@
 #include "holberton.h"
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+char *str_concat(char *s1, char *s2)
+{
+	char *stringcomb;
+	int cstr1 = 0, cstr2 = 0, contadorcomb, totalcomb, sustr = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[cstr1])
+	{
+		cstr1++;
+	}
+	while (s2[cstr2])
+	{
+		cstr2++;
+	}
+	totalcomb = (cstr1 + cstr2 + 1);
+	stringcomb = malloc(sizeof(*stringcomb) * totalcomb);
+	for (contadorcomb = 0; contadorcomb <= totalcomb; contadorcomb++)
+	{
+		if (contadorcomb < cstr1)
+		{
+			stringcomb[contadorcomb] = s1[contadorcomb];
+		}
+		else
+		{
+			stringcomb[contadorcomb] = s2[sustr];
+			sustr++;
+		}
+	}
+	return (stringcomb);
+}
