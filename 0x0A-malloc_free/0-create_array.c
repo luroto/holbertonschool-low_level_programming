08a645ed42573b1b8698a41d0255eaf060253068
Lucia Rodriguez <774@holbertonschool.com>
2019-03-06 00:01:53 +0000

    uploading the zero task, fixing issues according to betty analysis

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,6 +1,13 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ **create_array - creates an array with size defined from a variable on main
+ *
+ *@size: number of positions
+ *@c: character for filling the array
+ *Return: provided by the exercise
+ */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int con;
@@ -12,10 +19,9 @@ char *create_array(unsigned int size, char c)
 	}
 	str = malloc(sizeof(c) * size);
 	if (str == NULL)
-        {
-                return (NULL);
-        }
-
+	{
+		return (NULL);
+	}
 	for (con = 0; con < size; con++)
 	{
 		str[con] = c;
