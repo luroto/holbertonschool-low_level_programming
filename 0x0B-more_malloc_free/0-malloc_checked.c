195661b4e877c055b3b2f91b0833d5b4275a6fd8
Lucia Rodriguez <774@holbertonschool.com>
2019-03-07 21:09:43 +0000

    uploading for fixing checks

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -10,9 +10,9 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *pointer;
+	void *pointer;
 
-	pointer = malloc(sizeof(b));
+	pointer = (void *) malloc(b);
 	if (pointer == NULL)
 	{
 		exit(98);
