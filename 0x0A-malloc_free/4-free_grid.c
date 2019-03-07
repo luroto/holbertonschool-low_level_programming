a7208faec93cbd88844cb02faa5ac41778f9bab8
Lucia Rodriguez <774@holbertonschool.com>
2019-03-07 02:53:35 +0000

    fixing betty and another minor errors

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -3,26 +3,26 @@
 #include <stdlib.h>
 
 /**
+ *free_grid - frees an array
  *
- *
- *
- *
+ *@grid: points to an array of chars
+ *@height: indicates the number of rows in the array
  *
  */
 void free_grid(int **grid, int height)
 {
 	int itera1, itera2;
 
-	/*if (*grid == NULL)
+	if (*grid == NULL)
 	{
 		return;
-		}*/
+	}
 	for (itera1 = 0; itera1 < height; itera1++)
 	{
 		while (grid[itera2])
 		{
-			free (grid[itera1]);
-			free (grid[itera2]);
+			free(grid[itera1]);
+			free(grid[itera2]);
 			itera2++;
 		}
 	}
