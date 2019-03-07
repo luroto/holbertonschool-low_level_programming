3e0d91a52c4578c800441960edcc380461288a65
Lucia Rodriguez <774@holbertonschool.com>
2019-03-07 20:46:32 +0000

    uploading the first task

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -2,25 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ **malloc_checked - allocates memory
  *
+ *@b: it collects the main input info
  *
- *
- *
- *
- *
- *
- *
+ *Return: It returns the pointer
  */
 void *malloc_checked(unsigned int b)
 {
 	int *pointer;
 
 	pointer = malloc(sizeof(b));
-/**	
-if (pointer == NULL)
+	if (pointer == NULL)
 	{
 		exit(98);
 	}
-*/
 	return (pointer);
 }
