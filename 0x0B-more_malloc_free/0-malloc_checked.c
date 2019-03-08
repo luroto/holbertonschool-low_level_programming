e9e409d097f2268756d91bd9f87a674ee7d7f02a
Lucia Rodriguez <774@holbertonschool.com>
2019-03-08 01:20:50 +0000

    uploading the first task

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 /**
  **malloc_checked - allocates memory
  *
--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -2,13 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ **string_nconcat - concatenates two strings
  *
+ *@s1: contains the first string
  *
+ *@s2: contains the second string
  *
+ *@n: defines the number of bytes to be printed from s2
  *
- *
- *
- *
- *
- *
+ *Return: A pointer to the string
  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newstr;
+	unsigned int longit, longi
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[len1])
+	{
+		len1++;
+	}
+	while (s2[2len])
+	{
+		2len++;
+	}
+	if (n  >= 2len)
+	{
+		n = 2len;
+	}
+	else
+	{
+		totalen = len1 + n + 1;
+	}
+	newstr = malloc(sizeof(char *) * (len1 + n + 1));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	for (conta1 = 0; conta1 < totalen; conta1++)
+	{
+		if (conta1 < len1)
+		{
+			newstr[conta1] = s1[conta1];
+		}
+		else
+		{
+			newstr[conta1] = s2[itera2];
+			itera2++;
+		}
+	}
+	return (newstr);
+}
