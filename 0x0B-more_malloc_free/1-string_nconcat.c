aa960fc0deedd5799a25d5a7eda72485053cb80c
Lucia Rodriguez <774@holbertonschool.com>
2019-03-08 22:55:19 +0000

    uploading the task 1 again

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ unsigned int cstr1 = 0, cstr2 = 0, contadorcomb, totalcomb = 0, sustr = 0;
 	if (n <= cstr2)
 		cstr2 = n;
 	totalcomb = (cstr1 + cstr2 + 1);
-	stringcomb = malloc(sizeof(char) * totalcomb);
+	stringcomb = malloc(sizeof(*stringcomb) * totalcomb);
 	if (stringcomb == 0)
 		return (NULL);
 	for (contadorcomb = 0; contadorcomb < totalcomb; contadorcomb++)
