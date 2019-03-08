2b4fec5d9bbf9f202cb611eda686f4f77669a7a1
Lucia Rodriguez <774@holbertonschool.com>
2019-03-08 23:12:55 +0000

    uploading the first task

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -13,21 +13,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *stringcomb;
 unsigned int cstr1 = 0, cstr2 = 0, contadorcomb, totalcomb = 0, sustr = 0;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
 	while (s1[cstr1])
 		cstr1++;
 	while (s2[cstr2])
 		cstr2++;
 	if (n <= cstr2)
-		cstr2 = n - 1;
+		cstr2 = n;
 	totalcomb = (cstr1 + cstr2 + 1);
-	stringcomb = malloc(sizeof(*stringcomb) * totalcomb);
-	if (stringcomb == 0)
+	stringcomb = malloc(sizeof(stringcomb) * totalcomb);
+	if (stringcomb == NULL)
 		return (NULL);
-	for (contadorcomb = 0; contadorcomb < totalcomb; contadorcomb++)
+	for (contadorcomb = 0; contadorcomb < totalcomb - 1; contadorcomb++)
 	{
 		if (contadorcomb < cstr1)
 		{
