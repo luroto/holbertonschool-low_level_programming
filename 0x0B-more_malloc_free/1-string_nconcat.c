54f13a8a1584698a71f9a684729448dd9152d6e5
Lucia Rodriguez <774@holbertonschool.com>
2019-03-08 22:46:45 +0000

    uploading the first task

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -3,13 +3,9 @@
 #include <stdlib.h>
 /**
  **string_nconcat - concatenates two strings
- *
-ΩΩ≈ *@s1: contains the first string
- *
+ *@s1: contains the first string
  *@s2: contains the second string
- *
  *@n: defines the number of bytes to be printed from s2
- *
  *Return: A pointer to the string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -18,31 +14,19 @@ char *stringcomb;
 unsigned int cstr1 = 0, cstr2 = 0, contadorcomb, totalcomb = 0, sustr = 0;
 
 	if (s1 == 0)
-	{
 		s1 = "";
-	}
 	if (s2 == 0)
-	{
 		s2 = "";
-	}
 	while (s1[cstr1])
-	{
 		cstr1++;
-	}
 	while (s2[cstr2])
-	{
 		cstr2++;
-	}
 	if (n <= cstr2)
-	{
 		cstr2 = n;
-	}
 	totalcomb = (cstr1 + cstr2 + 1);
 	stringcomb = malloc(sizeof(char) * totalcomb);
 	if (stringcomb == 0)
-	{
 		return (NULL);
-	}
 	for (contadorcomb = 0; contadorcomb < totalcomb; contadorcomb++)
 	{
 		if (contadorcomb < cstr1)
