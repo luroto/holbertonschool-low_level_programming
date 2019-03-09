8487270cd9d0e3b91b17aa3566d27a14638a7281
Lucia Rodriguez <774@holbertonschool.com>
2019-03-09 01:52:50 +0000

    uploading the second task

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ **_calloc - allocates memory for an array using malloc
+ *@nmemb: number of elements in the array
+ *@size: size on bytes
+ *Return: the pointer to an array
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *memall, contador;
+	char *memall;
+	unsigned int contador;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	memall = malloc(size * nmemb);
 	if (memall == NULL)
 		return (NULL);
-	return (memall);
-	for (contador = 0; contador <= nmemb; contador++)
+	for (contador = 0; contador < (size * nmemb); contador++)
 	{
 		memall[contador] = 0;
 	}
+	return (memall);
 }
