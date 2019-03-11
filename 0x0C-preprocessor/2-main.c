677aab652fa6683c3bfbaf335f6f52b9dcbeda37
Lucia Rodriguez <774@holbertonschool.com>
2019-03-11 19:26:52 +0000

    uploading the second task

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
 #include <stdlib.h>
 #ifndef __FILE__
 #define __FILE__
 #endif
+/**
+ *main - prints the name of the file it was compiled from
+ *Return: it is not neccesary
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
