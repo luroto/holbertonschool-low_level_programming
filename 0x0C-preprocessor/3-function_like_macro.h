6ae0b0ccbc03d7c39cae25dcc9e97ae59dcea5a6
Lucia Rodriguez <774@holbertonschool.com>
2019-03-11 20:42:35 +0000

    uploading the third task with betty

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO
-#define _3_FUNCTION_LIKE_MACRO
+#ifndef _3_FUNCTION_LIKE_MACRO_
+#define _3_FUNCTION_LIKE_MACRO_
 #define ABS(X) ((X) < (0) ? ([X * -1]) : (X))
 #endif
