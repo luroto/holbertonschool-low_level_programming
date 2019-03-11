54d060b771d0bd513a0d8492ea4ebda2b4d84507
Lucia Rodriguez <774@holbertonschool.com>
2019-03-11 23:31:52 +0000

    uploading the header

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ *struct dog - define parameters for functions
+ *@name: Dog's name
+ *@age: Dog's age
+ *@owner: Name of dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
