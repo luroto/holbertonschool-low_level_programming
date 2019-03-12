f0b258aa256553c125d5010fb8dd1b51d4cbdd0c
Lucia Rodriguez <774@holbertonschool.com>
2019-03-12 02:23:46 +0000

    uploading the first task with all the checkers

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
 *init_dog - function that prints a struct
 *struct dog: collects info about our dog
@@ -9,7 +10,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
