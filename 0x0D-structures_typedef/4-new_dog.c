6d33c2ac21d5ca7ac7ffc18baba73fb7bd11393d
Lucia Rodriguez <774@holbertonschool.com>
2019-03-13 02:04:49 +0000

    uploading the fourth again

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -59,7 +59,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = _strdup(owner);
 	if (new_dog->owner == NULL)
 	{
-		free(name);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
