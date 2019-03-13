667471f6bb802f8350f7e7c7dd5b6bedc9141fc6
Lucia Rodriguez <774@holbertonschool.com>
2019-03-13 18:48:22 +0000

    uploading the zero task and fixing checkers

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
- *
+ *print_name - prints a name
+ *@name: source string
+ *@f: pointer to some functions
+ *Return: it is a void function
  */
 void print_name(char *name, void (*f)(char *))
 {
