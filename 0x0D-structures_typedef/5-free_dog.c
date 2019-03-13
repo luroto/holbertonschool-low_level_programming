0ecf774da18ffa390c6998af7500ef976d650ced
Lucia Rodriguez <774@holbertonschool.com>
2019-03-13 02:17:08 +0000

    uploading the fifth task

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,7 +1,17 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - frees all the dogs
+ *@d: pointer for the struct
+ *Return: It's a void function
+ */
 void free_dog(dog_t *d)
 {
-	free(new_dog);
-	free(dog_t);
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
 }
