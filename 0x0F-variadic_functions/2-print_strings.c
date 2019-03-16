610f32ab8fa255cebb6b0db42fac7a2ece7a83e1
Lucia Rodriguez <774@holbertonschool.com>
2019-03-16 17:10:30 +0000

    uploading the second task to fix some check

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -15,13 +15,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(listofstr, n);
 	for (i = 0; i < n; i++)
 	{
-		if (listofstr == NULL)
+		if (listofstr != NULL)
 		{
-			printf("%p", va_arg(listofstr, char *));
+			printf("%s", va_arg(listofstr, char *));
 		}
 		else
 		{
-			printf("%s", va_arg(listofstr, char *));
+			printf("%p", va_arg(listofstr, char *));
 		}
 		if (separator != NULL && i < (n - 1))
 		{
