9da844265e6c6fe8229a388d0463c549da0e526a
Lucia Rodriguez <774@holbertonschool.com>
2019-03-16 01:47:45 +0000

    uploading the first task and the updated header

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *print_numbers - Prints numbers, as function name indicates
+ *@separator: String to be printed between numbers
+ *@n: number of arguments
+ *Return: It's a void function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numeritos;
+	unsigned int x;
+
+	va_start(numeritos, n);
+	for (x = 0; x <= n; x++)
+	{
+		printf("%d", va_arg(numeritos, int));
+		if (separator != NULL && x != n)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(numeritos);
+	printf("\n");
+}
