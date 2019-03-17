1b8e576f30fadc32a0beb8ac2ec9ffbe81fd0358
Lucia Rodriguez <774@holbertonschool.com>
2019-03-17 00:10:23 +0000

    uploading the 3rd task and variadic functions header

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ *print_all - prints anything
+ *@format: type of arguments
+ *Return: It's a void function
+ */
 void print_all(const char * const format, ...)
 {
 	va_list cualquiera;
