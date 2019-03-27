2f3758d60c73da7f233237bf3ff42d2bdfd6481d
Lucia Rodriguez <774@holbertonschool.com>
2019-03-27 00:48:51 +0000

    [Feature] Uploading the third task and an updated version of header file

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ **add_nodeint_end - adds a node at the end of the list
+ *@head: the start of the list
+ *@n: value to be stored at n variable in list struct
+ *Return: the address of that node
+ *
+ *
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nodeatend, *aux;
+
+	nodeatend = malloc(sizeof(listint_t));
+	if (nodeatend == NULL)
+	{
+		free(nodeatend);
+		return (NULL);
+	}
+	nodeatend->next = NULL;
+	nodeatend->n = n;
+	if (*head == NULL)
+	{
+		*head = nodeatend;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+		{
+			aux = aux->next;
+		}
+		aux->next = nodeatend;
+	}
+	return (*head);
+}
