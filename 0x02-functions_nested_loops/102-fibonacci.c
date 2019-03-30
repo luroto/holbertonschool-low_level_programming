ab43775242dc5d576d06a00b3ac846db521e67d5
Lucia Rodriguez <774@holbertonschool.com>
2019-03-30 22:48:52 +0000

    uploading the advanced task for this project

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,25 +7,23 @@
  */
 int main(void)
 {
-int c;
-long int f1, f2, f3;
+	int c;
+	long int f1 = 0, f2 = 1, f3;
 
-f1 = 0;
-f2 = 1;
-for (c = 0 ; c < 49 ; c++)
-{
-f3 = f1 + f2;
-f1 = f2;
-f2 = f3;
-if (c == 46)
-{
-printf("%ld", f3);
-}
-else
-{
-printf("%ld, ", f3);
-}
-}
-putchar('\n');
-return (0);
+	for (c = 0 ; c < 49 ; c++)
+	{
+		f3 = f1 + f2;
+		f1 = f2;
+		f2 = f3;
+		if (c == 48)
+		{
+			printf("%ld", f3);
+		}
+		else
+		{
+			printf("%ld, ", f3);
+		}
+	}
+	printf("\n");
+	return (0);
 }
