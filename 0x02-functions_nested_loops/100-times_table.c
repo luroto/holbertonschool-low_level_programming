7cddac0eab2cecb639184338a5b5a4b99c3a9cb9
Lucia Rodriguez <774@holbertonschool.com>
2019-03-31 00:37:07 +0000

    [Change] Updating the n times function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,11 +31,11 @@ void print_times_table(int n)
 					{
 					_putchar(',');
 					_putchar(' ');
-					}
-					if ((rows * (columns + 1)) < 10)
-						_putchar(' ');
+					if ((rows *(columns + 1)) < 10)
+					    _putchar(' ');
 					if ((rows *(columns + 1 )) < 100)
 					    _putchar(' ');
+					}
 			}
 			_putchar('\n');
 		}
