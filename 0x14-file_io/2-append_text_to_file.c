a78eb4f029d6d4f61ae396dd4144ea4f122b246f
Lucia Rodriguez <774@holbertonschool.com>
2019-04-02 23:18:24 +0000

    [Change] Updating finally

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -23,7 +23,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-		write(fd, text_content, strlen(text_content));
+		if (text_content != NULL)
+		{
+			write(fd, text_content, strlen(text_content));
+		}
 	close(fd);
 	return (1);
 }
