aa4c84e269158cfc618db7dac9412cc36db0cd2e
Lucia Rodriguez <774@holbertonschool.com>
2019-04-03 02:46:04 +0000

    [Feature] Uploading file for cp

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *cp- Emulates cp function
+ *
+ *@filesource: path to the file to be copied
+ *@filedest: path to the copied
+ *Return: it depends
+ */
+void cp(char *filesource, char *filedest)
+{
+
+}
