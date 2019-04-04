6021b5674da25dfdd1e9d10e1197262745502715
Lucia Rodriguez <774@holbertonschool.com>
2019-04-04 22:15:41 +0000

    [Feature] Uploading the third task

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,12 +1,49 @@
 #include "holberton.h"
 /**
- *cp- Emulates cp function
- *
- *@filesource: path to the file to be copied
- *@filedest: path to the copied
+ *main- Emulates cp function
+ *@ac: # of arguments
+ *@av: arguments from the command line
  *Return: it depends
  */
-void cp(char *filesource, char *filedest)
+int main(int ac, char **av)
 {
+	int fdorigin, fddestiny, sizeall, alldest;
+	char buf[1024];
 
+	if (ac != 3)
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	fdorigin = open(av[1], O_RDONLY);
+	if (fdorigin == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s", av[1]),
+			exit(98);
+	fddestiny = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fddestiny == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s", av[2]),
+			exit(99);
+	sizeall = read(fdorigin, buf, 1024);
+	if (sizeall == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s", av[1]),
+			exit(98);
+	while (sizeall != 0)
+	{
+		alldest = write(fddestiny, buf, sizeall);
+		if (alldest == -1)
+			dprintf(STDOUT_FILENO,
+				"Error: Can't write to %s", av[2]),
+				exit(99);
+		sizeall = read(fdorigin, buf, 1024);
+		if (sizeall == -1)
+			dprintf(STDOUT_FILENO,
+				"Error: Can't read from file %s", av[1]),
+			exit(98);
+	}
+	alldest = close(fdorigin);
+	if (alldest == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fdorigin),
+		exit(100);
+	alldest = close(fddestiny);
+	if (alldest == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fddestiny);
+	return (0);
 }
