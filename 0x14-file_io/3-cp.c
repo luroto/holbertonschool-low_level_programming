a64e8aca581bf64d47dcf900202edfb2aacb6ed5
Lucia Rodriguez <774@holbertonschool.com>
2019-04-05 01:21:05 +0000

    [change] Updating the cp code

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -12,39 +12,39 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n"),
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
 	fdorigin = open(av[1], O_RDONLY);
 	if (fdorigin == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]),
-			exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			av[1]), exit(98);
 	fddestiny = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fddestiny == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]),
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
 			exit(99);
 	sizeall = read(fdorigin, buf, 1024);
 	if (sizeall == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]),
-			exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			av[1]), exit(98);
 	while (sizeall != 0)
 	{
 		alldest = write(fddestiny, buf, sizeall);
 		if (alldest == -1)
-			dprintf(STDOUT_FILENO,
+			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", av[2]),
 				exit(99);
 		sizeall = read(fdorigin, buf, 1024);
-                if (sizeall == -1)
-                        dprintf(STDOUT_FILENO,
-                                "Error: Can't read from file %s\n", av[1]),
-                                exit(98);
+		if (sizeall == -1)
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", av[1]),
+				exit(98);
 	}
 	alldest = close(fdorigin);
 	if (alldest == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fdorigin),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdorigin),
 		exit(100);
 	alldest = close(fddestiny);
 	if (alldest == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fddestiny);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fddestiny);
 	return (0);
 }
