eb95347fe567f0ebda459511c3e96feb335e323d
Lucia Rodriguez <774@holbertonschool.com>
2019-05-03 19:47:43 +0000

    [Update]Uploading the first task

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - Prints the number of elements on a linked list
+ *@h: Pointer to another function
+ * Return: Number of elements on a linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+size_t iteraele = 0;
 
+while (h != NULL)
+{
+	h = h->next;
+	iteraele++;
+}
+return (iteraele);
 }
