44aae35bf06b1cd49f314068af9a07725401837b
Lucia Rodriguez <774@holbertonschool.com>
2019-05-14 01:45:18 +0000

    [Uploading the 4th task]

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *free_list - frees a linked list
+ *@head: first node of the linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+
+	}
+}
