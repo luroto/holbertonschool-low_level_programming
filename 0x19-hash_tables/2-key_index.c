fa813e2b5ebdc879d982d34fbbebe453c0f2a400
Lucia Rodriguez <774@holbertonschool.com>
2019-05-31 21:39:01 +0000

    [Update] Uploading the second task

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -8,13 +8,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int itera = 0;
 	unsigned long int index;
 
-	while (key && key[itera])
-	{
-		index = (hash_djb2(key)) % size;
-		itera++;
-	}
+	index = (hash_djb2(key)) % size;
 	return (index);
 }
