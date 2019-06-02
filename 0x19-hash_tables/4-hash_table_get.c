34bec4fd9d289c133c807c0195d5934326a0df59
Lucia Rodriguez <774@holbertonschool.com>
2019-06-02 23:32:17 +0000

    [Update] Uploading the 4th task with conditions fixed

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,11 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *aux;
 
+	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
 		return(NULL);
 	}
-	index = key_index((unsigned char *)key, ht->size);
 	aux = ht->array[index];
 	while (aux != NULL)
 	{
