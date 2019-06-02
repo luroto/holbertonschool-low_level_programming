680578d6c6507aad83276e9a112b0672fe8db8c4
Lucia Rodriguez <774@holbertonschool.com>
2019-06-02 23:19:41 +0000

    Uploading the 3rd task

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,11 +15,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	aux = ht->array[index];
-	if (strcmp(aux->key, key) == 0)
+	while (aux != NULL)
 	{
-		aux->value = strdup(value);
-		ht->array[index] = aux;
-		return (1);
+		if (strcmp(aux->key, key) == 0)
+		{
+			aux->value = strdup(value);
+			return (1);
+		}
+		aux = aux->next;
 	}
 	newnode = malloc(sizeof(newnode));
 	if (newnode == NULL)
@@ -33,6 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (1);
 	}
 	newnode->next = aux;
-	ht->array[size] = newnode;
+	ht->array[index] = newnode;
 	return (1);
 }
