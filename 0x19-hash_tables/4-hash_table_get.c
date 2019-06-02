a38db69569144ab9f5efedead83473c418b7b0db
Lucia Rodriguez <774@holbertonschool.com>
2019-06-02 23:39:48 +0000

    [Update]Uploading the 4th task

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	aux = ht->array[index];
 	while (aux != NULL)
