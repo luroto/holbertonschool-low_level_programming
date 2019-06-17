b290684668578fb0b52096046aa6808d2b333ad9
Lucia Rodriguez <774@holbertonschool.com>
2019-06-17 22:22:14 +0000

    [Update] Uploading the sort.h file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -30,8 +30,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+int swapping(int *array, size_t size, size_t ai, size_t aj)
 #endif /* SORT_H */
