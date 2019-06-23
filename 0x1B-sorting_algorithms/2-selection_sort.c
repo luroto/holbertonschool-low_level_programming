6e2b5233429f8f9c024e761f96a914afa5bea359
Lucia Rodriguez <774@holbertonschool.com>
2019-06-23 22:52:05 +0000

    [Update] Uploading the second task again

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,30 +1,12 @@
 #include "sort.h"
-/**
- * swapping - Do swaps
- *@array: Given array
- *@size: Size of the array
- *@ai: First iterator
- *@minind: second iterator, contains the minimum
- *Return: 0 if sucessful
- */
-int swapping(int *array, size_t size, size_t ai, size_t minind)
-{
-	size_t aux;
-
-	aux = array[ai];
-	array[ai] = array[minind];
-	array[minind] = aux;
-	print_array(array, size);
-	return (0);
-}
-/**
+ /**
  *selection_sort - An implementation of this algorithm
  *@array: Given array
  *@size: The size of that array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t ai, aj, minind, pingu;
+	size_t ai, aj, minind, pingu, aux;
 
 	for (ai = 0; ai < (size - 1); ai++)
 	{
@@ -40,7 +22,10 @@ void selection_sort(int *array, size_t size)
 		}
 		if (pingu == 1)
 		{
-		swapping(array, size, ai, minind);
+			aux = array[ai];
+			array[ai] = array[minind];
+			array[minind] = aux;
+			print_array(array, size);
 		}
 	}
 }
