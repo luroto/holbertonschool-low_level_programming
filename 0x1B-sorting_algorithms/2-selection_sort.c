4be1aadfbf07b368338d1c7976d273ab249672f0
Lucia Rodriguez <774@holbertonschool.com>
2019-06-23 22:59:13 +0000

    [Update] Uploading the second task again with minimum number in array's size

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
- /**
- *selection_sort - An implementation of this algorithm
+
+/**
+ * selection_sort - An implementation of the algorithm
  *@array: Given array
  *@size: The size of that array
  */
@@ -8,24 +9,27 @@ void selection_sort(int *array, size_t size)
 {
 	size_t ai, aj, minind, pingu, aux;
 
-	for (ai = 0; ai < (size - 1); ai++)
+	if (array != NULL && size >= 2)
 	{
-		minind = ai;
-		pingu = 0;
-		for (aj = ai + 1; aj < size; aj++)
+		for (ai = 0; ai < (size - 1); ai++)
 		{
-			if (array[aj] < array[minind])
+			minind = ai;
+			pingu = 0;
+			for (aj = ai + 1; aj < size; aj++)
 			{
-				minind = aj;
-				pingu = 1;
+				if (array[aj] < array[minind])
+				{
+					minind = aj;
+					pingu = 1;
+				}
 			}
-		}
-		if (pingu == 1)
-		{
-			aux = array[ai];
-			array[ai] = array[minind];
-			array[minind] = aux;
-			print_array(array, size);
+			if (pingu == 1)
+			{
+				aux = array[ai];
+				array[ai] = array[minind];
+				array[minind] = aux;
+				print_array(array, size);
+			}
 		}
 	}
 }
