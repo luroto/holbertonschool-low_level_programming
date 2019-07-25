07e50b7268ae00913f577acfaaf98bcd9048bf4e
Lucia Rodriguez <774@holbertonschool.com>
2019-07-24 21:34:04 -0500

    [Update] Uploading the 15th task

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -7,11 +7,16 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int fully;
+
 	if (tree == NULL)
 		return (0);
-	if (!(tree->left) && !(tree->right))
+	if ((tree->left == NULL) && (tree->right == NULL))
 		return (1);
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	if (tree->left != NULL && tree->right != NULL)
+	{
+	     fully = binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+	     return(fully);
+	}
 	return (0);
 }
