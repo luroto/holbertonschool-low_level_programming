3a2436e566ae4b1b5f13ab8de264ab36ba2c571f
David Latorre <david@latorredev.com>
2019-07-25 22:05:55 -0500

    [Feature] check post order binary tree

--- 0x1D-binary_trees/8-binary_tree_postorder.c
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -5,7 +5,7 @@
 * @tree: given tree
 * @func: given function
 */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
 	{
