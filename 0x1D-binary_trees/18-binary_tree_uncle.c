175e1120ef0261d05985e3e7569ff6b3446975dc
David Latorre <david@latorredev.com>
2019-07-26 18:44:44 -0500

    [Feature] Add uncle in binary tree

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -2,9 +2,9 @@
 #include "17-binary_tree_sibling.c"
 
 /**
- * binary_tree_sibling - Finds the sibling of a node
- *@node: Node to find its sibling
- * Return: a pointer to the sibling node, otherwise NULL
+ * binary_tree_uncle - Finds the sibling of a node
+ *@node: Node to find its uncle
+ * Return: a pointer to the uncle node, otherwise NULL
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
