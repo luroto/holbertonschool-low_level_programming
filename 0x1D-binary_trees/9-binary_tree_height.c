2e37a46cfeabdbff42959b5729b3ef11c225ca3c
Lucia Rodriguez <774@holbertonschool.com>
2019-07-26 21:04:02 -0500

    [Update] Updating the 9th file

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -8,14 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_depth;
+	size_t right_depth;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_depth = 0;
-	size_t right_depth = 0;
-
 	left_depth = tree->left != NULL ? 1 + binary_tree_height(tree->left) : 0;
 	right_depth = tree->right != NULL ? 1 + binary_tree_height(tree->right) : 0;
 
