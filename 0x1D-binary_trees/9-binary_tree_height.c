0719f94433f34ca43d973cbab67e73163799cac6
David Latorre <david@latorredev.com>
2019-07-26 19:48:21 -0500

    [Feature] found the height of a binary tree

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -16,8 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_depth = 0;
 	size_t right_depth = 0;
 
-	left_depth = tree->left != NULL ? 1 + binary_tree_height(tree->left): 0;
-	right_depth = tree->right != NULL ? 1 + binary_tree_height(tree->right): 0;
+	left_depth = tree->left != NULL ? 1 + binary_tree_height(tree->left) : 0;
+	right_depth = tree->right != NULL ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (left_depth > right_depth ? left_depth : right_depth);
 }
