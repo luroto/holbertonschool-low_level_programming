6f9cc2b2b42e59bf825a9e93c637cc7c870cdaf6
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-28 19:22:36 +0000

    [Fix] Fixing conditional: iterator must be equal to size at the end

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		if (array[i] != value)
 			printf("Value checked array[%lu] = [%i]\n", i, array[i]);
