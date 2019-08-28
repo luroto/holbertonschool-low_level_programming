2c3ea740b8b38744ae6ede80f7c4538835d1e688
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-28 19:27:23 +0000

    [Fix] It wasn conditional, just output fixes, reordering steps

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,11 +16,10 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (array[i] != value)
-			printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-		else
+	     	printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
