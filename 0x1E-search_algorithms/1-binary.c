ffc0ba068f649c4df8444eb43159f14057f45660
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-30 06:15:06 +0000

    [Fix] Finally fixed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,22 +6,25 @@
  *@array: array of integers to be searched
  *@size: size of the array
  *@value:value to be found
- *Return: index if value is founded, -1 if array is null or not present on array
+ *Return: index if value is founded, -1 if array is null or
+ *not present on array
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, ran, mita, paraar;
+	size_t i, ran, mita, paraar, index;
 
 	ran = size - 1;
-
+	paraar = 0;
+	index = -1;
+	i = 0;
 	if (array == NULL)
-		return(-1);
-	for (i = 0; i < size; i++)
+		return (index);
+	while (i < size)
 	{
 		if (i > ran)
 			return (-1);
-		mita = (i + ran)/ 2;
-		paraar = i;
+		mita = (i + ran) / 2;
+		printf("Searching array: ");
 		while (paraar <= ran)
 		{
 			if (paraar < ran)
@@ -31,11 +34,14 @@ int binary_search(int *array, size_t size, int value)
 			paraar++;
 		}
 		if (array[mita] < value)
+		{
 			i = mita + 1;
-		if (array [mita] > value)
+			paraar = i;
+		}
+		if (array[mita] > value)
 			ran = mita - 1;
 		if (array[mita] == value)
-			return(mita);
+			return (mita);
 	}
-	return(-1);
+	return (index);
 }
