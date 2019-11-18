2f12e0515376c403476a98c3575be1b9fe59c20a
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-11-18 20:38:57 +0000

    [Fix] Uploading the 101 code, betty approved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
-int main (void){
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return(1);
+/**
+ * main - Prints to standard error without printf or puts
+ * Return: 1
+ *
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+	return (1);
 }
