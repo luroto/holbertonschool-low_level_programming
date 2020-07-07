6a10f0d33e957d2ab4eb6284d238a88c2731b89a
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-07 01:04:42 +0000

    [Fix] Fixing conditionals

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sumando, sumatoria;
+	int i, j, sumando, sumatoria;
 
 	if (argc == 1)
 	{
@@ -19,15 +19,20 @@ int main(int argc, char **argv)
 	}
 	for (i = 1; i < argc; i++)
 	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] >= 48 && argv[i][j] >= 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
 		sumando = atoi(argv[i]);
 		if (sumando != 0)
 		{
 			sumatoria += sumando;
-		}
-		else
-		{
-			printf("%s\n", "Error");
-			return (1);
 		}
 	}
 	printf("%d\n", sumatoria);
