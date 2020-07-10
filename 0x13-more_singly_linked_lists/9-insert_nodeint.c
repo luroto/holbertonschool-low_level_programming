acac95e610e3a9623da4e0e7a3b9f057c7ae7a96
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-10 18:55:25 +0000

    [Fix] Fixing conditional

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	initial = *head;
 	while (*head != NULL)
 	{
-		if (tofound == idx)
+		if (tofound == (idx-1))
 		{
 			newnode = malloc(sizeof(*head));
 			if (newnode == NULL)
