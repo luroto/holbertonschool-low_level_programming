d44fe4e1f4365eca692f78e3dd57ddee96b4a04c
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-11 18:05:01 +0000

    [Fix] Uploading the third task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+/**
+ **add_node_end - Add a new node at the end.
+ *@head: Pointer to another pointer (node)
+ *@str: String source
+ *Return: the address of the new node or NULL if the function fails
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nuevonodo, *temp;
+	unsigned int itera = 0;
+
+	while (str[itera] != '\0')
+	{
+		itera++;
+	}
+	nuevonodo = malloc(sizeof(list_t));
+	if (nuevonodo == NULL)
+	{
+		free(nuevonodo);
+		return (NULL);
+	}
+	nuevonodo->str = strdup(str);
+	nuevonodo->len = itera;
+	nuevonodo->next = NULL;
+	if (*head == NULL)
+	{
+		*head = nuevonodo;
+		return (nuevonodo);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = nuevonodo;
+	return (nuevonodo);
+}
