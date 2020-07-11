6031afcab34b3ff0c023f0a6d1184fea6d8c78fa
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-11 22:34:27 +0000

    [Fix] Trying something with head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			newnode->next = aux->next;
 			aux->next = newnode;
+			*head = aux;
 			return (newnode);
 		}
 		aux = aux->next;
