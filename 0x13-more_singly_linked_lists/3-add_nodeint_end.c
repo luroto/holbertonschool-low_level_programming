f335c14b583eef00605f2cf7e56e328532561c99
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-11 23:34:35 +0000

    [Fix] Uploading the third file for testing

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,6 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = nodeatend;
+		return (*head);
 	}
 	else
 	{
@@ -34,6 +35,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			aux = aux->next;
 		}
 		aux->next = nodeatend;
+		return (nodeatend);
 	}
-	return (*head);
 }
