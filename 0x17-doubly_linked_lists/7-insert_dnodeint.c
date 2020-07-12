e7cb7cf3f02c30cb0ee50689ce49310c527b11e4
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-12 17:11:04 +0000

    [Fix] Check carefully the code first: connect head to newnode via prev

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		newnode->next = *h;
 		newnode->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = newnode;
 		*h = newnode;
 		return (*h);
 	}
