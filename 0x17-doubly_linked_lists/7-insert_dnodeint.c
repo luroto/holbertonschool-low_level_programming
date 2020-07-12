2c3a7cd3bf9a77646237c43673409c81281e1deb
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-12 16:26:44 +0000

    [Fix] Fix the aux->next->prev conditional in case of last node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			newnode->next = aux->next;
 			newnode->prev = aux;
-			aux->next->prev = newnode;
+			if (aux->next != NULL)
+				aux->next->prev = newnode;
 			aux->next = newnode;
 			return (newnode);
 		}
