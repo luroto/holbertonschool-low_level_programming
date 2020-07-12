11fe3bb5dd3b9c4c7f50b9e778a679c38c063835
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-12 16:42:33 +0000

    [Fix] Removing conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,8 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			newnode->next = aux->next;
 			newnode->prev = aux;
-			if (aux->next != NULL)
-				aux->next->prev = newnode;
+			aux->next->prev = newnode;
 			aux->next = newnode;
 			return (newnode);
 		}
