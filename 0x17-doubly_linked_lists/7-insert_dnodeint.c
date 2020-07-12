5ba9d23e59ac45d6db5b88ffa42f7c1445880b1f
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-12 03:06:52 +0000

    [Feature] Uploading the add at given index node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,43 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index -
- *
- * Return: 
+ * insert_dnodeint_at_index - Insert a node in a given position
+ *@h: Double pointer for the entire linked list
+ *@idx: Index for inserting the node
+ *@n: value to be added
+ *Return: the created node, otherwise NULL
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int i = 0;
+	dlistint_t *newnode, *aux;
 
+	if (h == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+
+	if (idx == 0)
+	{
+		newnode->next = *h;
+		newnode->prev = NULL;
+		*h = newnode;
+		return (*h);
+	}
+	aux = *h;
+	while (aux != NULL)
+	{
+		if (i == (idx - 1))
+		{
+			newnode->next = aux->next;
+			newnode->prev = aux;
+			aux->next = newnode;
+			return (newnode);
+		}
+		i++;
+		aux = aux->next;
+	}
+	return (NULL);
 }
