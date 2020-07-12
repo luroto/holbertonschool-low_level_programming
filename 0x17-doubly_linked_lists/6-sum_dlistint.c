d04175dff81d6ee64a7cf5a8056d811eef10cc78
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-12 02:24:06 +0000

    [Update] Uploading the sixth file

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - Adds all the values on a linked list
+ *@head: First node of the LL
+ *Return: sum of all node's values, otherwise 0
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int i = 0;
 
+	while (head != NULL)
+	{
+		i += head->n;
+		head = head->next;
+	}
+	return (i);
 }
