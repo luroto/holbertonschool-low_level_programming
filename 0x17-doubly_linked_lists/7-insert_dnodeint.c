ab83212e73f60fd0e3827662714a3753949ec402
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-12 03:12:18 +0000

    [Fix] Fix the aux->next->prev conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,6 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			newnode->next = aux->next;
 			newnode->prev = aux;
+			aux->next->prev = newnode;
 			aux->next = newnode;
 			return (newnode);
 		}
